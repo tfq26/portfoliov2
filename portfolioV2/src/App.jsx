d2e2623143f367e212b7259c0f9fe49dca2a4467
tfq26 <108446173+tfq26@users.noreply.github.com>
2025-01-22 12:54:09 -0600

    Added Hero and Navbar Component, need to address the css issue

---
+++ portfolioV2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            {/* Add other components */}
+        </div>
+    );
+};
+
+export default App;
